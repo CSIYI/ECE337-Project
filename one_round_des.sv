61119a961314ea63ac1a6ab679889d6e6a0cb950
CSIYI <cai71@purdue.edu>
2016-04-14 14:12:45 -0400

    Added files via upload

--- one_round_des.sv
+++ one_round_des.sv
@@ -1,22 +1,27 @@
 module one_round_des
   (
-   input wire [63:0] data,
+   input wire [31:0] left_reg,
+   input wire [31:0] right_reg,
    input wire [47:0] key,
-   output wire one_round_rlt
+   output wire [31:0] left_reg_out,
+   output wire [31:0] right_reg_out
    );
 
-   wire [31:0] left_reg, right_reg, left_reg_nxt, right_reg_nxt;
-   always_ff @ (posedge clk, negedge n_rst) begin:
-      if (n_rst== 1'b0) begin
-	 left_reg <= 32'b0;
-	 right_reg <= 32'b0;
-      end
-      else begin
-	 left_reg <= left_reg_nxt;
-	 right_reg <= right_reg_nxt;
-      end
+ 
+   wire [47:0] 	      expand_rlt, Xor;
+   wire [31: 0]       Sbox_rlt, shrink_rlt;
+   
+   assign left_reg_out = right_reg;
+   expansion_p_box expand32to48 (.data(right_reg), .48bit(exapnd_rlt));
+   assign Xor = expand_rlt ^ key;
+   s_box_all sbox(.data(Xor), .s_rlt(Sbox_rlt));
+   32bit_p_box shrink (.data(Sbox_rlt), .32bit(shrink_rlt));
+   assign right_reg_out = shink_rlt ^ left_reg;
+   
+endmodule // one_round_des
+
       
-   always_comb begin next_reg
+      
      
       
 
